79aff0f851414f04a1cda6117ad54ce60380d863
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-01-31 19:10:47 -0300

    pegando tipos de roupa 23

--- js.js
+++ js.js
@@ -1,27 +1,47 @@
 let modelo;
 let gola;
 let tecido;
-
 function tipo1(pega){
-    const constante=document.querySelector('.grande .borda')
-    if(constante !== null){
-        constante.classList.remove('borda')
+    modelo=document.querySelector('.grande .borda')
+    if(modelo !== null){
+        modelo.classList.remove('borda')
     }
     pega.classList.add('borda')
+
+    modelo=pega.querySelector('.grande .direita img')
+    console.log(modelo)
+}
+function tipo2(pega2){
+    gola=document.querySelector('.grande2 .borda')
+    if(gola !== null){
+        gola.classList.remove('borda')
+    }
+    pega2.classList.add('borda')
+
+    gola=pega2.querySelector('.grande2 .direita img')
+    console.log(gola)
 }
 
-function tipo2(pega){
-    const constante=document.querySelector('.grande2 .borda')
-    if(constante !== null){
-        constante.classList.remove('borda')
+function tipo3(pega3){
+   tecido=document.querySelector('.grande3 .borda')
+    if(tecido !== null){
+        tecido.classList.remove('borda')
     }
-    pega.classList.add('borda')
-}
-function tipo3(pega){
-    const constante=document.querySelector('.grande3 .borda')
-    if(constante !== null){
-        constante.classList.remove('borda')
-    }
-    pega.classList.add('borda')
+    pega3.classList.add('borda')
+
+    tecido=pega3.querySelector('.grande3 .direita img')
+    console.log(tecido)
 }
 
+/*function pegaLink(){
+    const linkbonito = document.querySelector('.link').value
+    console.log(linkbonito)
+    linkbonito.value = '';
+    console.log(linkbonito)
+} */
+
+
+function pegaLink(){
+
+}
+    
